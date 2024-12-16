e03896c42e7bc3a0d160156f36b5793af521fa8b
neondodongo <willirmler@me.com>
2024-12-16 15:52:17 -0600

    feat(post): added modal

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostList.module.css";
+import Modal from "./Modal";
 
 const PostList = () => {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler(event) {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -17,10 +23,14 @@ const PostList = () => {
 
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
       <ul className={styles.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Brittany" body="Weeeeeee" />
