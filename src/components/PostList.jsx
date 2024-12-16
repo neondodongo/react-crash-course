f96ff559af3f13263c99c911f626989c15614199
neondodongo <willirmler@me.com>
2024-12-16 14:50:33 -0600

    feat(post): new post component, initial impl

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostList.module.css";
 
 function PostList() {
   return (
-    <ul className={styles.posts}>
-      <Post author="Will" body="React is OK." />
-      <Post author="Brittany" body="Weeeeeee" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={styles.posts}>
+        <Post author="Will" body="React is OK." />
+        <Post author="Brittany" body="Weeeeeee" />
+      </ul>
+    </>
   );
 }
 
