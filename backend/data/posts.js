bf3e9bb12b02c3db0ae1244e1d6b238f5d0118a0
neondodongo <willirmler@me.com>
2024-12-26 12:23:15 -0600

    feat(backend): create post json if not exists

--- backend/data/posts.js
+++ backend/data/posts.js
@@ -1,14 +1,30 @@
-const fs = require('node:fs/promises');
+const fs = require("node:fs/promises");
+const path = require("node:path");
+
+const filePath = path.resolve("posts.json");
+
+async function ensureFileExists() {
+  try {
+    await fs.access(filePath); // Check if the file exists
+  } catch (error) {
+    // If the file does not exist, create it with an empty posts array
+    await fs.writeFile(filePath, JSON.stringify({ posts: [] }, null, 2));
+  }
+}
 
 async function getStoredPosts() {
-  const rawFileContent = await fs.readFile('posts.json', { encoding: 'utf-8' });
+  await ensureFileExists();
+  const rawFileContent = await fs.readFile(filePath, { encoding: "utf-8" });
   const data = JSON.parse(rawFileContent);
   const storedPosts = data.posts ?? [];
   return storedPosts;
 }
 
 function storePosts(posts) {
-  return fs.writeFile('posts.json', JSON.stringify({ posts: posts || [] }));
+  return fs.writeFile(
+    filePath,
+    JSON.stringify({ posts: posts || [] }, null, 2)
+  );
 }
 
 exports.getStoredPosts = getStoredPosts;
